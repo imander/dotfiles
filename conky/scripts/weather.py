796efa680ad9eb6cd992f00f35efda7d2ca7af72
imander <andrsn84@gmail.com>
2019-09-28 20:25:36 -0700

    Remove weather graphics

--- conky/scripts/weather.py
+++ conky/scripts/weather.py
@@ -4,7 +4,7 @@ import os
 
 location_file = os.getenv('HOME') + '/.config/conky/location'
 forecast_file = os.getenv('HOME') + '/.config/conky/forecast'
-image_dir = os.getenv('HOME') + '/.config/conky/images'
+# image_dir = os.getenv('HOME') + '/.config/conky/images'
 
 r = requests.get('http://ip-api.com/json')
 out = json.loads(r.text)
@@ -21,8 +21,8 @@ forecast = requests.get(r['properties']['forecast']).text
 with open(forecast_file, 'w+') as f:
     f.write(forecast)
 
-data = json.loads(forecast)
-for n in range (0, 3):
-    img_data = requests.get(data['properties']['periods'][n]['icon']).content
-    with open(image_dir + f'/image{n}.png', 'wb') as handler:
-        handler.write(img_data)
+# data = json.loads(forecast)
+# for n in range (0, 3):
+#     img_data = requests.get(data['properties']['periods'][n]['icon']).content
+#     with open(image_dir + f'/image{n}.png', 'wb') as handler:
+#         handler.write(img_data)
