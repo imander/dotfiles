f3310d2ff2e57f2281ef31c4a2de6599e0191bf2
imander <andrsn84@gmail.com>
2021-02-06 11:43:01 -0800

    add ubuntu packages

---
+++ packages/ubuntu/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+SCRIPT_DIR=$(dirname $0)
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+function log() {
+  msg=${1:-}
+  printf "$(date -u) ${GREEN}INFO${NC} $msg\n"
+}
+
+function install() {
+  pkg=${1:-}
+  if [[ -n "$pkg" ]]; then
+    apt -qq list "$pkg" 2>/dev/null | grep -q 'installed' || (
+      log "installing $pkg"
+      sudo apt-get install -y "$pkg"
+    )
+  fi
+}
+
+sudo apt-get update
+while read package; do
+  install "$package"
+done <"${SCRIPT_DIR}/packages"
+
+exit 0
